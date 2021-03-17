c282f12aded678dca6e75f22e5ff3d84807abdf9
Braulio <hermeztercero@gmail.com>
2021-03-17 16:01:56 -0600

    Solucionamos el error y corrimos el comando build

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,8 +1,9 @@
 const common = require('./webpack.common.js');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
+    mode: 'development',
     module:{
         rules: [        
             {
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,36 +1,36 @@
+
 const common = require('./webpack.common.js');
-const merge = require('webpack-merge');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const {merge} = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJS = require('terser-webpack-plugin');
 
-const extractSass = new ExtractTextPlugin({
-    filename: 'dist/css/[name].[contenthash].css'
-});
 
-module.exports = merge(common, {
-    output:{
-        publicPath: '.'
+module.exports = merge(common,{
+    mode: 'production',
+    plugins:[
+      new MiniCssExtractPlugin({
+        filename: 'dist/css/[name].[contenthash].css'
+      })
+    ],
+    optimization:{
+      minimizer: [new TerserJS(),new OptimizeCssAssetsPlugin({})]
     },
-    module:{
-        rules: [        
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use:[
-                        {loader: 'css-loader', options: {minimize:true} },
-                        {loader: 'sass-loader'}
-                    ]  
-                })
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {loader: 'html-loader', options: {minimize:true, attrs: false}}
-                ]
-            }
+    output: {
+        publicPath:'.'
+    },
+    module: {
+        rules: [
+          {
+            test: /.scss$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          },
+          {
+            test:/.html$/,
+            use: {loader:'html-loader', options:{minimize:true, attributes:false}}
+          }
         ]
-    },
-    plugins: [extractSass]
+      },
 });
 
-// minimize: true -> lo que hace es compactar el archivo para que se vea minificado
-// attrs: false que no modifique los atributos 
+// https://webpack.js.org/configuration/mode/
